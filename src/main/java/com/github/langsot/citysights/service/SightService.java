1daf241592ec7125c4728ec1c1394e96d3eb333a
Eugene Belezki <langsot1337@gmail.com>
2022-05-26 14:57:07 +0700

    #1 frame project

--- src/main/java/com/github/langsot/citysights/service/SightService.java
+++ src/main/java/com/github/langsot/citysights/service/SightService.java
@@ -1,5 +1,6 @@
 package com.github.langsot.citysights.service;
 
+import com.github.langsot.citysights.entity.City;
 import com.github.langsot.citysights.entity.Sight;
 import com.github.langsot.citysights.repo.SightRepository;
 import lombok.AllArgsConstructor;
@@ -15,5 +16,9 @@ public class SightService {
 
     public List<Sight> getAll() {
         return repository.findAll();
+    }
+
+    public void save(Sight sight) {
+        repository.save(sight);
     }
 }
